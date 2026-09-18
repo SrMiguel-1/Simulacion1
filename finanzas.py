093aa6c5ceeaab54c3e6b86343c6a60600584de2
Miguel Angel Jaimes Bernal <miguelangeljaimesbernal@gmail.com>
2026-09-18 17:49:05 -0500

    codigo modificado

--- finanzas.py
+++ finanzas.py
@@ -1,8 +1,14 @@
 def fib(numero):
     x,y= 0,1
     list=[]
+    resultado=0
+    resultado= resultado * x
     while x<=numero:
         list.append(x)
         x, y=y, x+y
     return list
+    print("resultado de suceción de fubinacci: ", resultado)
+
+
 fib(8)
+
